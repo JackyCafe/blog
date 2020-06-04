e900a7124419251bf598fa7ed358ea54e4ad0a4e
powerworker1234@gmail.com <powerworker1234@gmail.com>
2020-06-04 14:15:32 +0800

    models setup

--- app1/models.py
+++ app1/models.py
@@ -5,6 +5,7 @@ from django.db import models
 # Create your models here.
 class Group(models.Model):
     name = models.CharField(max_length=30,null=False)
+    created = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
         return self.name
@@ -35,7 +36,9 @@ class Classroom(models.Model):
 class Activity(models.Model):
     id = models.AutoField(primary_key=True)
     name = models.CharField(max_length=20,null=False,verbose_name='活動名稱')
-    group = models.ManyToManyField(Group,on_delete=models.CASCADE,related_name='activity')
+    group = models.ManyToManyField(Group,related_name='activity')
+    classroom = models.ForeignKey(Classroom,on_delete=models.CASCADE,related_name='activity')
+    created = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
         return self.name
@@ -51,7 +54,8 @@ class Card(models.Model):
     description= models.TextField(null=True,verbose_name='描述')
     feature = models.TextField(null=True,verbose_name='特點')
     activity = models.ForeignKey(Activity,on_delete=models.CASCADE,related_name='card')
-    group = models.ManyToManyField(Group, on_delete=models.CASCADE, related_name='card')
+    group = models.ManyToManyField(Group,  related_name='card')
+    created = models.DateTimeField(auto_now_add=True)
 
 
     def __str__(self):
